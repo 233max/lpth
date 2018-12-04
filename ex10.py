d5f0e68231a250b8177aad21762f8e7603d9618a
233max <jming.dong@gmail.com>
2018-12-04 22:58:37 +0800

    dec 4 last commit

--- ex10.py
+++ ex10.py
@@ -1,15 +1,15 @@
-tabby_cat = "\tI'm tabbed in."
+tabby_cat = "\tI'm tabbed in {}."
 persian_cat = "I'm split\non a line"
 backslash_cat = "I'm \\ a \\ cat"
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
 \t* Cat food
 \t* Fishies
 \t* Catnip\n\t* Grass
-"""
+'''
 
-print(tabby_cat)
+print(tabby_cat.format("tabby cat"))
 print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+fifth = input()
+script, first, second, third, fourth= argv
+
+print("The script is called:", script)
+print("Your first varibale is:", first)
+print("Your second varibale is:", second)
+print("Your third variable is:", third)
+print("Your fourth variable is:", fourth)
+print("Your fifth variable is:", fifth)
